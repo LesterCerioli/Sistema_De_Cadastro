dc28a802dbf3a06e37b458a2e70151ff0690ce3f
unknown <lesterlucasit@hotmail.com.com>
2018-09-30 17:59:57 -0300

    Import with controllers and Angular JS

---
+++ Cadastro_Angular/Controllers/Pessoa_FisicaController.cs
@@ -0,0 +1,25 @@
+﻿using Cadastro_Angular.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cadastro_Angular.Controllers
+{
+    public class Pessoa_FisicaController : Controller
+    {
+        //GET Pessoa_Fisica/GETPessoa_Fisica
+        // métpdp para listar Pessoa Física
+        public JsonResult GetPessoa_Fisica()
+        {
+            using (var db = new cadastros1Entities())
+            {
+                List<Pessoa_Fisica> listarPessoa_Fisica = db.Pessoa_Fisica.ToList();
+                return Json(listarPessoa_Fisica, JsonRequestBehavior.AllowGet);
+            }
+
+                    
+        }
+    }
+}
